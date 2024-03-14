fc870cac7b4b9588ab3cda6a545b37d390fa3dae
skifton <antoniestories@gmail.com>
2024-03-14 22:16:33 +0100

    added redirect to main page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/(//)+", // Match any number of consecutive slashes
+        destination: "/", // Redirect to the root path
+        statusCode: 301, // Use 301 status code for permanent redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
