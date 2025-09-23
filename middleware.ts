1012abdff66729ae03e9b4e7fac138e4720b6395
Mayki73 <136011712+Mayki73@users.noreply.github.com>
2025-09-23 18:55:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,1 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export function middleware(_req: NextRequest) {
-  return new NextResponse('Not Found', {
-    status: 404,
-    headers: { 'Cache-Control': 'no-store' },
-  })
-}
-
-export const config = {
-  matcher: '/:path*', // перехватываем все пути
-}
